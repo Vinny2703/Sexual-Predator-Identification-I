ba8c5ab57c543e0ee25eb86618034cf06a79d402
Vinny2703 <92690810+Vinny2703@users.noreply.github.com>
2021-12-17 20:11:21 +0100

    Update Sexual-Predator-Identification-I.R

--- Sexual-Predator-Identification-I.R
+++ Sexual-Predator-Identification-I.R
@@ -1,17 +1,9 @@
 library("XML")
 library("methods")
-#Korpus einfügen (evtl. über Console, wenn nicht an korrekter Stelle)
+#Korpus einfÃ¼gen (evtl. Ã¼ber Console, wenn nicht an korrekter Stelle)
 corpus <- xmlParse(file = "pan12-sexual-predator-identification-training-corpus-2012-05-01.xml")
 #path <- readline(prompt = "Input File: ")
 #corpus <- xmlParse(file = path)
 
-#Korpus bereinigen: entferne Konversationen unter 10 Nachrichten und mit einzelnen Personen (noch unvollständig)
-corpusclean <- xpathApply(corpus, "//conversation[./message[@line>='10'] and ./message/author!=./message/author]")
-
-#Korpus fromatieren?
-
-
-#xpathApply(corpus, "//conversation[count(child::*/child::author)=2]")
-
-#xpathApply(corpus, "//conversation[count(./message/author[not(./message/author)])=10]")
-#xpathApply(corpus, "//conversation[count(./message/author[not(. = ../following-sibling::/message/author)])=10]")
+#Korpus bereinigen: entferne Konversationen unter 10 Nachrichten und mit !=2 Personen 
+corpusclean <- xpathApply(corpus, "//conversation[./message[@line>='10'] and count(./message[not(author = following-sibling::message/author)])=2]")
