d6e2cd508027f340345b7acb0ae9ac1d8f06c347
Vinny2703 <92690810+Vinny2703@users.noreply.github.com>
2022-02-08 15:02:17 +0100

    Create Model112.R

---
+++ Model112.R
@@ -0,0 +1,50 @@
+#Model112 eigener Corpus und Conversationsstart Feature
+#Variablen
+epochs<-30
+batch_s<-8
+lr<-0.0001
+word_vector <- layer_input(shape = (NULL))
+other_feature <- layer_input(shape = (extraInput)) 
+a_embedding<- layer_embedding(one_hot_vectorsize,embedd_v_dim)(autorvec)
+conv1D_1 <- layer_conv_1d(40, 1,padding = 'same', activation ='relu' )(a_embedding) 
+conv1D_3 <- layer_conv_1d(40,3,padding = 'same',activation ='relu')(a_embedding)
+conv1D_5 <- layer_conv_1d(40,5,padding = 'same',activation ='relu')(a_embedding)
+conv1D_7 <- layer_conv_1d(40,7,padding = 'same', activation ='relu')(a_embedding) 
+x<- layer_concatenate(list(conv1D_1, conv1D_3,conv1D_5,conv1D_7))
+autor_embedding <-layer_embedding()
+
+model112 <- keras_model_sequential()
+model112%>% word_vector
+embedding %>% 
+  x %>%
+  y<-layer_max_pooling_1d()%>%
+  layer_concatenate(list(y, other_feature))%>%
+  layer_dense(units=20, activation ='tanh' )%>%
+  layer_dropout(0,5) %>%
+  layer_dense(units=1, activation = 'sigmoid')
+
+#Training
+#Metric festlegen
+metric <- c(metric_precision(name = 'precision'), metric_recall(name = 'recall'))
+#compile
+model112%>% compile(
+  loss = 'binary_crossentropy',
+  optimizer=optimizer_adam(lr=lr),
+  metrics=metric
+)
+#fit
+tensorboard("logs/run_a")
+hist1<-fit_generator(
+  #object,
+  epochs = epochs,
+  batch_size =batch_s,
+  callbacks = list(
+    callback_model_checkpoint(file.path(path,"model112_checkpoint.hdf5"), period = 10, save_freq = 'epoch'),
+    callback_tensorboard(log_dir = file.path(path,"model112_logs"))
+))
+save_model_hdf5(model112,"model112/")
+
+#testen
+#Model112 laden
+model112<-load_model_hdf5("model112/")
+
