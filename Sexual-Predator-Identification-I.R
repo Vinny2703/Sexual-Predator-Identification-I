ead6fca300210b094877f8ef641e3baeb9237107
Vinny2703 <92690810+Vinny2703@users.noreply.github.com>
2022-02-24 18:32:53 +0100

    Delete Sexual-Predator-Identification-I.R

--- Sexual-Predator-Identification-I.R
+++
@@ -1,9 +0,0 @@
-library("XML")
-library("methods")
-#Korpus einfügen (evtl. über Console, wenn nicht an korrekter Stelle)
-corpus <- xmlParse(file = "pan12-sexual-predator-identification-training-corpus-2012-05-01.xml")
-#path <- readline(prompt = "Input File: ")
-#corpus <- xmlParse(file = path)
-
-#Korpus bereinigen: entferne Konversationen unter 10 Nachrichten und mit !=2 Personen 
-corpusclean <- xpathApply(corpus, "//conversation[./message[@line>='10'] and count(./message[not(author = following-sibling::message/author)])=2]")
